a8457aa13eb602c078410062793338c9413cbc7c
elgraphica <elferenczi@outlook.com>
2023-02-21 14:52:25 +0000

    function to load current question,display teh answer chices, add 10 point to correct answer, subtract 10 seconds on incorrrect answer complete

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -52,4 +52,50 @@ const quizQuestions = [
       }
     }, 1000);
   
-    
+    // Initial question index to 0
+let currentQuestionIndex = 0;
+  
+// Define the function to load the current question
+function loadQuestion() {
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+
+  // Display the current question
+  document.getElementById("question-title").textContent = currentQuestion.question;
+
+  // Display the answer choices for the current question
+  const choicesContainer = document.getElementById("choices");
+  choicesContainer.innerHTML = "";
+  for (let i = 0; i < currentQuestion.choices.length; i++) {
+    const choice = currentQuestion.choices[i];
+    const choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceButton.addEventListener("click", function() {
+      if (choice === currentQuestion.answer) {
+        // If the answer is correct, add 10 points to the score and load the next question
+        score += 10;
+        currentQuestionIndex++;
+        if (currentQuestionIndex < quizQuestions.length) {
+          loadQuestion();
+        } else {
+          // If all questions have been answered, end the quiz
+          endQuiz();
+        }
+      } else {
+        // If the answer is incorrect, subtract 10 seconds from the timer and load the next question
+        timeLeft -= 10;
+        currentQuestionIndex++;
+        if (currentQuestionIndex < quizQuestions.length) {
+          loadQuestion();
+        } else {
+          // If all questions have been answered, end the quiz
+          endQuiz();
+        }
+      }
+    });
+    choicesContainer.appendChild(choiceButton);
+  }
+}
+
+// Load the first question
+loadQuestion();
+}
